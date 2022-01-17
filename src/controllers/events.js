6a07131a403993bc0f27d9071ea7760e801dce1b
Soumyajit Das <mepioneiro@gmail.com>
2022-01-17 13:58:12 +0530

    modify request body and responses

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -6,8 +6,10 @@ module.exports.get = async (req, res) => {
   try {
     const event = await eventdb.findById(
       req.params.id,
-      "_id title description image form data"
+      "_id title description image form date"
     );
+
+    if (!event) return res.status(404).json({ error: "No Events Found" });
 
     res.status(200).json(event);
   } catch (error) {
@@ -20,7 +22,7 @@ module.exports.getAll = async (req, res) => {
   try {
     const events = await eventdb.find(
       {},
-      "_id title description image form data"
+      "_id title description image form date"
     );
 
     res.status(200).json({ events, length: events.length });
@@ -32,12 +34,19 @@ module.exports.getAll = async (req, res) => {
 // Create New Event
 module.exports.add = async (req, res) => {
   try {
-    const { error } = validator.event(req.body.event);
+    const { error } = validator.event(req.body);
     if (error) return res.status(406).json({ error: "Invalid Event Data" });
 
-    const newEvent = await eventdb.create(req.body.event);
+    const newEvent = await eventdb.create(req.body);
 
-    res.status(200).json({ event: newEvent });
+    res.status(200).json({
+      _id: newEvent._id,
+      title: newEvent.title,
+      description: newEvent.description,
+      image: newEvent.image,
+      form: newEvent.form,
+      date: newEvent.date,
+    });
   } catch (error) {
     res.status(500).json({ error: "Cannot Create Event" });
   }
@@ -46,16 +55,26 @@ module.exports.add = async (req, res) => {
 // Update Specific Event Based On It's ID
 module.exports.update = async (req, res) => {
   try {
+    const { error } = validator.event(req.body);
+    if (error) return res.status(406).json({ error: "Invalid Event Data" });
+
     const updatedEvent = await eventdb.findByIdAndUpdate(
       req.params.id,
-      req.body.event,
+      req.body,
       { new: true, runValidators: true }
     );
 
     if (!updatedEvent)
       return res.status(404).json({ message: "Cannot Find Event" });
 
-    res.status(200).json({ event: updatedEvent });
+    res.status(200).json({
+      _id: updatedEvent._id,
+      title: updatedEvent.title,
+      description: updatedEvent.description,
+      image: updatedEvent.image,
+      form: updatedEvent.form,
+      date: updatedEvent.date,
+    });
   } catch (error) {
     res.status(500).json({ error: "Cannot Update Event" });
   }
@@ -69,7 +88,14 @@ module.exports.delete = async (req, res) => {
     if (!deletedEvent)
       return res.status(404).json({ message: "Cannot Find Event" });
 
-    res.status(200).json({ event: deletedEvent });
+    res.status(200).json({
+      _id: deletedEvent._id,
+      title: deletedEvent.title,
+      description: deletedEvent.description,
+      image: deletedEvent.image,
+      form: deletedEvent.form,
+      date: deletedEvent.date,
+    });
   } catch (error) {
     res.status(500).json({ error: "Cannot Delete Event" });
   }
